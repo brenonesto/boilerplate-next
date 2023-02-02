4b5281485e9e38fd9e8bbd9cfa22fa66d1c53a6d
Breno Nesto <breno.nesto@unifei.edu.br>
2023-02-02 12:28:11 -0300

    feat: Creating nextJS boilerplate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === 'production'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
 
-module.exports = nextConfig
+module.exports = withPWA({
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  }
+})
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+import * as s from './styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, ReactJS, NextJS e Styled Components'
+}) => (
+  <s.Wrapper>
+    <s.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <s.Title>{title}</s.Title>
+    <s.Description>{description}</s.Description>
+    <s.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    />
+  </s.Wrapper>
 )
 
 export default Main
